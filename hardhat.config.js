3fef3ce728d19301f0e0558122dece98380fe3e3
Jose Perez <mobilefl44@gmail.com>
2021-07-12 02:43:56 -0400

    matic example

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,14 @@
-require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-// task("accounts", "Prints the list of accounts", async () => {
-//   const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
 
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -17,33 +16,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- module.exports = {
-  defaultNetwork: "matic",
-  networks: {
-    hardhat: {
-    },
-    matic: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [PRIVATE_KEY]
-    }
-  },
-  solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
-  mocha: {
-    timeout: 20000
-  }
-}
+module.exports = {
+  solidity: "0.8.6",
+};
 
